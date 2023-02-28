731130d651d439439639e0409ddf9283742ffc06
Kyle Wong <Kwong498@gmail.com>
2023-02-27 22:17:11 -0500

    associations added, models changed to singular

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,25 +4,24 @@ const Users = require('./users');
 const Products = require('./products');
 
 const Cart = db.define('cartItem', {
-
-    qty: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-    },
-    userId: {
-        type: Sequelize.INTEGER,
-        references: {
-            model: Users,
-            key: 'id'
-        }
-    },
-    productId: {
-        type: Sequelize.INTEGER,
-        references: {
-            model: Products,
-            key: 'id'
-        }
+  qty: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Users,
+      key: 'id'
     }
+  },
+  productId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Products,
+      key: 'id'
+    }
+  }
 });
 
-module.exports = Cart
+module.exports = Cart;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,20 +2,46 @@ const Users = require('./users');
 const Products = require('./products');
 const Reviews = require('./reviews');
 const Orders = require('./orders');
+const Categories = require('./categories');
+const WishLists = require('./wishList');
+const UserAddresses = require('./userAddress');
+const Cart = require('./cart');
+const UserPayments = require('./userPaymentDetails');
+
 // import all models
 
 // define relationships here
+UserAddresses.hasMany(Users); //! Can have multiple users that live together
+Users.hasOne(UserAddresses);
+
+Users.hasOne(Cart);
+Cart.hasOne(Users);
+
+Cart.hasMany(Products);
+Products.hasOne(Cart);
+
+WishLists.hasOne(Users);
+Users.hasOne(WishLists);
+
+WishLists.hasMany(Products);
+Products.hasOne(WishLists);
+
 Users.hasMany(Orders);
-Orders.belongsTo(Users);
+Orders.hasOne(Users);
 
-Orders.hasMany(Products);
-Products.belongsTo(Orders);
+Users.hasMany(Reviews);
+Reviews.hasOne(Users);
 
+Reviews.hasOne(Products);
 Products.hasMany(Reviews);
-Reviews.belongsTo(Products);
 
 Orders.hasMany(Products);
-Products.belongsTo(Orders);
+Products.hasOne(Orders);
+
+Products.hasOne(Categories);
+Categories.hasOne(Products);
+
+//!UserPaymentDetails
 
 module.exports = {
   Users,
