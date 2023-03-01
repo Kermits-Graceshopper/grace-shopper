5149ec2781b239890dafa5e5271cee3b9f06b289
daveontherun <116976689+daveontherun@users.noreply.github.com>
2023-03-01 16:21:55 -0500

    db model whitespace cleanup

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,17 @@
 const db = require("./db");
-const { Users, Products, Orders, Reviews, Categories, WishLists, UserAddresses, Cart, UserPayments, CartItems, OrderItems } = require("./models");
+const { Users, Products, Orders, Reviews, Categories, UserAddresses, Cart, } = require("./models");
 const seed = require("../../script/seed");
 
 const syncDb = async () => {
   try {
-    // await db.sync({ force: true});
+    await db.sync({ force: true});
     console.log("SUCCESS, db has been synced");
-    await db.seed()
+    // await db.seed()
   } catch (e) {
     console.log("ERROR IN CATCH OF syncDb FUNCTION: ", e);
   }
 };
-// syncDb();
+syncDb();
 
 module.exports = {
     db,
