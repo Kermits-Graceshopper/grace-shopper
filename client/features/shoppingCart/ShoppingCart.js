9679792df1a54a9f168c33b8aea2285ffa59721c
bvilla203 <ben.tan.villafranca@gmail.com>
2023-03-01 19:40:40 -0500

    created basic layout of shopping cart

--- client/features/shoppingCart/ShoppingCart.js
+++ client/features/shoppingCart/ShoppingCart.js
@@ -1,45 +1,73 @@
-import React from 'react'
-import Navbar from '../navbar/Navbar';
-import SpecializedNavbar from '../specializedNavbar/SpecializedNavbar';
-import Footer from '../footer/Footer';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Button from 'react-bootstrap/Button';
 
 const ShoppingCart = () => {
+    let cartProducts = [
+        { name: "Elden Ring" },
+        {
+            name: "Spiderman"
+        }
+    ]
+
+//   const dispatch = useDispatch();
+//   const cartProducts = useSelector(selectCartProducts); //! IMPORT FROM REDUX
+
+//   useEffect(() => {
+//     dispatch(fetchCartProductsAsync()); //! IMPORT FROM REDUX
+//   }, []);
+    
+    
   return (
-    <div>
-      shopping cart
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+    <div className="cartDivParent">
+      <div className="cartDiv">
+        <div></div>
+        {cartProducts.length > 0
+          ? cartProducts.map((product) => {
+              return <div>{product.name}</div>;
+            })
+          : 'There is nothing in your cart'}
+        <div>
+          <div className="checkoutButton d-grid gap-4">
+            <Button variant="success" size="lg">
+              Block level button
+            </Button>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
