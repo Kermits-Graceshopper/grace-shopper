3a5a6279759901179931af4ccd2e49ecf3d1a1f3
Kyle Wong <Kwong498@gmail.com>
2023-03-06 13:42:00 -0500

    Cart working with stripe, checkout button displays only when there are cart items

--- client/features/shoppingCart/ShoppingCart.js
+++ client/features/shoppingCart/ShoppingCart.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "../api/axios";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from '../api/axios';
+import { useNavigate } from 'react-router-dom';
 import {
 	fetchAllProductsAsync,
 	selectAllProducts
-} from "../../app/reducers/allProductsSlice";
-import { selectUser } from "../../app/reducers/userSlice";
-import { Button } from "react-bootstrap";
+} from '../../app/reducers/allProductsSlice';
+import { selectUser } from '../../app/reducers/userSlice';
+import { Button } from 'react-bootstrap';
 
 const ShoppingCart = () => {
 	const [cart, setCart] = useState([]);
-  let runningTotal = 0;
+	let runningTotal = 0;
 	// let cartProducts = [
 	//   { id: 1, name: 'Elden Ring', price: 10.99, quantity: 2 },
 	//   { id: 2, name: 'Dark Souls', price: 10.99, quantity: 2 },
 	//   { id: 3, name: 'Diablo II', price: 10.99, quantity: 2 }
 	// ];
 	const products = useSelector(selectAllProducts);
-	// const navigate = useNavigate();
 	const user = useSelector(selectUser);
 	const isLoggedIn = user.isLoggedIn;
 	const dispatch = useDispatch();
-	//   const cartProducts = useSelector(selectCartProducts); //! IMPORT FROM REDUX
 
-	//   useEffect(() => {
-	//     dispatch(fetchCartProductsAsync()); //! IMPORT FROM REDUX
-	//   }, []);
+	const stripeItems = cart.map((item) => {
+		const product = products.find((product) => product.id === item.productId);
+		const price = parseFloat(product.price);
+		return {
+			id: item.productId,
+			name: product.name,
+			imageUrl: product.imageUrl,
+			price: price,
+			quantity: item.quantity
+		};
+	});
+	console.log('stripedata', stripeItems);
 
 	const handleCheckout = async () => {
 		try {
 			await axios
 				.post(
-					"/api/checkout",
+					'/api/checkout',
 					{
-						cart: cart
+						cart: stripeItems
 					},
 					{
 						headers: {
-							"Access-Control-Allow-Origin": "*"
+							'Access-Control-Allow-Origin': '*'
 						}
 					}
 				)
@@ -57,52 +64,58 @@ const ShoppingCart = () => {
 			let ip;
 			if (!isLoggedIn) {
 				await axios
-					.get("https://api.ipify.org")
+					.get('https://api.ipify.org')
 					.then((response) => (ip = response.data));
-				console.log("IP VARIABLE FROM WISHLIST.JS USEEFFECT: ", ip);
+				console.log('IP VARIABLE FROM WISHLIST.JS USEEFFECT: ', ip);
 			}
 			let data;
-			console.log("user: ", user);
-			console.log("isLoggedIn: ", isLoggedIn);
+			console.log('user: ', user);
+			console.log('isLoggedIn: ', isLoggedIn);
 			isLoggedIn
-				? (data = await axios.get("/api/cart", {
+				? (data = await axios.get('/api/cart', {
 						params: {
 							userId: user.id
 						}
 				  }))
-				: (data = await axios.get("/api/cart", {
+				: (data = await axios.get('/api/cart', {
 						params: {
 							guestId: ip
 						}
 				  }));
-			console.log("(cart) data.data: ", data.data);
+			// console.log('(cart) data.data: ', data.data);
 			setCart(data.data);
 		};
 		getCart();
 		dispatch(fetchAllProductsAsync());
 	}, []);
-	console.log("products: ", products);
-	console.log("cart: ", cart);
+	// console.log('products: ', products);
+	// console.log('cart: ', cart);
+
 	return (
 		<div className="changingBody">
 			<h1> Cart</h1>
 			<div className="productContainer">
 				{cart[0] ? (
-					cart.map((item) => (
+					cart.map((item) =>
 						products
-							? products.map(product => (
+							? products.map((product) =>
 									item.productId === product.id ? (
 										<div className="cartItem">
 											<h3>{product.name}</h3>
 											<img src={product.imageUrl} />
 											<h5>${product.price}</h5>
-                      <h5>Quantity: {item.quantity}</h5>
-                      <p className='priceCalc'>{runningTotal = runningTotal + product.price * item.quantity}</p>
+											<h5>Quantity: {item.quantity}</h5>
+											<p className="priceCalc">
+												{
+													(runningTotal =
+														runningTotal + product.price * item.quantity)
+												}
+											</p>
 										</div>
 									) : null
-							  ))
+							  )
 							: null
-          ))
+					)
 				) : (
 					<h2>Your cart is empty!</h2>
 				)}
@@ -112,19 +125,23 @@ const ShoppingCart = () => {
 					<div className="cartTitle">
 						<h2>
 							Order Summary <br />
-							{/* <div>{`Subtotal: (${cartProducts.length}items)`}</div> */}
 						</h2>
 						<hr />
 						<h2>Subtotal: ${runningTotal.toFixed(2)}</h2>
 					</div>
 					<hr />
-					<Button
-						className="coButton"
-						variant="primary"
-						size="lg"
-						onClick={handleCheckout}>
-						Proceed to checkout
-					</Button>
+					{cart[0] ? (
+						<Button
+							className="coButton"
+							variant="primary"
+							size="lg"
+							onClick={handleCheckout}
+						>
+							Proceed to checkout
+						</Button>
+					) : (
+						<></>
+					)}
 				</div>
 			</div>
 		</div>
--- server/api/checkout.js
+++ server/api/checkout.js
@@ -3,35 +3,36 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 router.post('/', async (req, res) => {
-  try {
-    const lineItems = req.body.cart.map((obj) => {
-      return {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: obj.name
-          },
-          unit_amount: obj.price * 100
-        },
-        quantity: obj.quantity
-      };
-    });
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
-      line_items: lineItems,
-      success_url: 'http://localhost:8080/success',
-      cancel_url: 'http://localhost:8080/cart'
-    });
-    // res.redirect(session.url);
+	try {
+		const lineItems = req.body.cart.map((obj) => {
+			return {
+				price_data: {
+					currency: 'usd',
+					product_data: {
+						name: obj.name
+					},
+					unit_amount: Math.round(obj.price * 100)
+					//! READS PRICE IN CENTS
+				},
+				quantity: obj.quantity
+			};
+		});
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ['card'],
+			mode: 'payment',
+			line_items: lineItems,
+			success_url: 'http://localhost:8080/success',
+			cancel_url: 'http://localhost:8080/cart'
+		});
+		// res.redirect(session.url);
 
-    res.json({
-      sessionUrl: session.url
-    });
-  } catch (e) {
-    console.log('some error');
-    console.log(e);
-  }
+		res.json({
+			sessionUrl: session.url
+		});
+	} catch (e) {
+		console.log('some error');
+		console.log(e);
+	}
 });
 
 module.exports = router;
