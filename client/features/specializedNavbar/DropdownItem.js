05314acd03d4cfe39e3de555ef0d54f089362786
bvilla203 <ben.tan.villafranca@gmail.com>
2023-03-06 00:13:13 -0500

    admin window closing functionality complete

--- client/features/specializedNavbar/DropdownItem.js
+++ client/features/specializedNavbar/DropdownItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DropdownItem = (props) => {
   return (
-    <li className="dropdownItem">
-      <img className="navIcon" src={props.img}></img>
-      <a > {props.text}</a>
-    </li>
+    <Link to="/home">
+      <li className="dropdownItem">
+        <img className="navIcon" src={props.img}></img>
+        <a> {props.text}</a>
+      </li>
+    </Link>
   );
 };
 
