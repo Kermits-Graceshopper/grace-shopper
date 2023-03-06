5a6ea5727598be7d3bdc70adfd18ab79f4fcbb2a
bvilla203 <ben.tan.villafranca@gmail.com>
2023-03-06 00:09:27 -0500

    admin window closing functionality complete

--- client/features/specializedNavbar/DropdownMenu.js
+++ client/features/specializedNavbar/DropdownMenu.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DropdownItem from './DropdownItem';
 
 const DropdownMenu = () => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    
+    let menuRef = useRef();
   const handleOpen = () => {
     setOpen(!open);
   };
+    
+    useEffect(() => {
+        let handler = (event) => {
+            if (!menuRef.current.contains(event.target)) {
+                setOpen(false);
+            }
+            
+        }
+        document.addEventListener('mousedown', handler);
+    })
   return (
-    <div className="menuContainer">
+    <div className="menuContainer" ref={menuRef}>
       <button className="menuButton" onClick={handleOpen}>
         <img className="icon" src="/img/gear.png" />
       </button>
