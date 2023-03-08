5f4fd3c54c803c06bfa0b3f9be119962bbc14609
bvilla203 <ben.tan.villafranca@gmail.com>
2023-03-07 19:35:44 -0500

    All Products Style Optimized

--- client/app/App.js
+++ client/app/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <div className="content-wrap">
       <Navbar />
       <SpecializedNavbar />
-        <div className='changingBody'>
+        <div>
           <AppRoutes />
         </div>
 
